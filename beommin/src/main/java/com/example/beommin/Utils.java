9bf72f3685b676a0fe04e1b13a8bfb70f382b6eb
beomsic <qjatjr29@khu.ac.kr>
2022-05-04 21:59:19 +0900

    연락처 포맷 코드 수정 및 패키지 위치 수정

--- beommin/src/main/java/com/example/beommin/Utils.java
+++ beommin/src/main/java/com/example/beommin/Utils.java
@@ -9,4 +9,10 @@ public class Utils {
         ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
         return new UUID(byteBuffer.getLong(),byteBuffer.getLong());
     }
+
+    public static String phoneFormat(String number) {
+        String regEx = "(\\d{3})(\\d{3,4})(\\d{4})";
+        number = number.replaceAll(" ","");
+        return number.replaceAll(regEx, "$1-$2-$3");
+    }
 }
